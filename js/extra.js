87a0661b7c3d16154e4992697592e3e1c69e3277
jovsil <jovsil@hotmail.com>
2024-11-29 15:39:47 -0600

    Add files via upload

--- js/extra.js
+++ js/extra.js
@@ -30,3 +30,26 @@ $(document).ready(function(){
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems, options);
   });
+  const audio = document.getElementById('miAudio');
+  let audioReproducido = false; // Evita múltiples reproducciones
+
+  const activarAudio = () => {
+    if (!audioReproducido) {
+      audio.muted = false; // Activa el sonido
+      audio.play().then(() => {
+        console.log("Audio reproducido");
+        audioReproducido = true; // Marca que ya se reprodujo
+        // Remueve todos los listeners después de la primera reproducción
+        document.removeEventListener('scroll', activarAudio);
+        document.removeEventListener('click', activarAudio);
+        document.removeEventListener('mousemove', activarAudio);
+      }).catch(error => {
+        console.error('Error al intentar reproducir el audio:', error);
+      });
+    }
+  };
+
+  // Escucha varios eventos para asegurar interacción del usuario
+  document.addEventListener('scroll', activarAudio);
+  document.addEventListener('click', activarAudio);
+  document.addEventListener('mousemove', activarAudio);
